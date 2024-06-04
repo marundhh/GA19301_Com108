4cd42c9c81b380b415f141acb44eda0ab8097f9b
Khash <141542010+RyoKatori@users.noreply.github.com>
2024-06-04 08:37:41 +0700

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
